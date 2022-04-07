c3cd9dba8bfaa465c7f236749499e95423ff48a2
Filipe Mutz <filipemtz@gmail.com>
2022-04-07 13:30:25 -0300

    removed files that should be ignored

--- pmtoolv2/settings.py
+++ pmtoolv2/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'scrum.apps.ScrumConfig',
     'django.contrib.admin',
     'django.contrib.auth',
